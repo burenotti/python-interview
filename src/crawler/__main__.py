3b0268615e57d2113d304d26e3399e9ecec26dd3
Artem <burenin.aa@yandex.ru>
2026-03-04 23:21:21 +0300

    Initial state

---
+++ src/crawler/__main__.py
@@ -0,0 +1,40 @@
+import uuid
+from uuid import uuid4
+import os
+import shutil
+import aiohttp
+from yarl import URL
+import asyncio
+from typing import cast
+import sys
+import pathlib
+from argparse import ArgumentParser, Namespace
+
+class CLIArguments(Namespace):
+    input_file: pathlib.Path
+    output_dir: pathlib.Path
+
+async def main() -> None:
+    parser = ArgumentParser(prog="web-crawler")
+    parser.add_argument("-i", "--input-file", type=pathlib.Path, help="input file path", dest="input_file")
+    parser.add_argument("-o", "--output-dir", type=pathlib.Path, help="output to save files", dest="output_dir")
+    args = cast(CLIArguments, parser.parse_args(sys.argv[1:]))
+
+    args.output_dir.mkdir(parents=True, exist_ok=True)
+
+    with open(args.input_file) as input_file:
+        input_urls_raw = input_file.readlines()
+        input_urls = [URL(url) for url in input_urls_raw]
+
+    for url in input_urls:
+        session = aiohttp.ClientSession()
+        response = await session.get(url)
+        response_content = await response.read()
+
+        filename_parts = str(uuid.uuid4()) + '.'.join(pathlib.Path(url.parts[-1]).suffixes)
+        output_path = args.output_dir / filename_parts
+        with open(output_path, 'wb') as output_file:
+            output_file.write(response_content)
+
+if __name__ == "__main__":
+    asyncio.run(main())
